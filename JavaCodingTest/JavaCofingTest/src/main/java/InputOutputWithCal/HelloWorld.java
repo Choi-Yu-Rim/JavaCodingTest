ddd25fbc89d51743447af1e4316c984ee0b8a847
choiyurum <iam_ip@naver.com>
2023-12-22 18:10:37 +0900

    commit2

--- JavaCodingTest/JavaCofingTest/src/main/java/InputOutputWithCal/HelloWorld.java
+++ JavaCodingTest/JavaCofingTest/src/main/java/InputOutputWithCal/HelloWorld.java
@@ -3,6 +3,6 @@ package InputOutputWithCal;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("dd");
+        System.out.println("dddddd");
     }
 }
