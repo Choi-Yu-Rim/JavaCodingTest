9c6a93cf0ae6bb63502ab4158d8f94ffa1a1f0d1
choiyurum <iam_ip@naver.com>
2023-12-23 19:46:37 +0900

    Systemin을 BufferedReader 로 읽어와서 계산하기

---
+++ JavaCodingTest/JavaCofingTest/src/main/java/If/CompareTwoNum.java
@@ -0,0 +1,12 @@
+package If;
+
+public class CompareTwoNum {
+    public static void main(String[] args) {
+        /*
+        * 1330.
+        * 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+        * */
+
+
+    }
+}
--- JavaCodingTest/JavaCofingTest/src/main/java/InputOutputWithCal/AplusB.java
+++ JavaCodingTest/JavaCofingTest/src/main/java/InputOutputWithCal/AplusB.java
@@ -1,7 +1,10 @@
 package InputOutputWithCal;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.Scanner;
 
 public class AplusB {
@@ -11,14 +14,22 @@ public class AplusB {
         *
         * 1. InputStream        : 바이트 단위로 문자를 읽어 들인다.
         * 2. InputStreamReader  : 문자 단위로 읽어 들인다.
-        * 3. BufferedReader     : 문자열을 읽어 들인다.
+        * 3. BufferedReader     : 문자열을 읽어 들인다. -> 엔터만 구분으로 해서 버퍼리더는 입력시 엔터로 구분 해야 함
         * 4. Scanner            : 위 3가지 보다 가장 쉽게 입력받을 수 있음 (공백(띄어쓰기) 또는 개행(줄 바꿈)을 기준으로 읽는다.)
         * */
-        Scanner ab = new Scanner(System.in);
 
-        int a = ab.nextInt();
-        int b = ab.nextInt();
+//        Scanner ab = new Scanner(System.in);
+//
+//        int a = ab.nextInt();
+//        int b = ab.nextInt();
+//
+//        System.out.println(a+b);
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
         System.out.println(a+b);
+
     }
 }
