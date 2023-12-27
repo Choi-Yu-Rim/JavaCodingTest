6db19c6341bf8c7220d67a4e425bcde78f2aab4b
choiyurum <iam_ip@naver.com>
2023-12-28 01:47:38 +0900

    텍스트 print 방법 3. 미완성 중간 커밋

--- JavaCodingTest/JavaCofingTest/src/main/java/codingTestForRang/print.java
+++ JavaCodingTest/JavaCofingTest/src/main/java/codingTestForRang/print.java
@@ -31,32 +31,58 @@ public class print {
 //        }
 
         // 방법 2.
-        String[][] arr1 = new String[5][5]; // 정방향
-        String[][] arr2 = new String[4][5]; // 역방향
+//        String[][] arr1 = new String[5][5]; // 정방향
+//        String[][] arr2 = new String[4][5]; // 역방향
+//
+//        for(int i=0; i < arr1.length; i++){
+//            for(int j=0; j < arr1[0].length; j++){
+//                if(i == j) {
+//                    arr1[i][j] = "O";
+//                }else {
+//                    arr1[i][j] = "o";
+//                }
+//                System.out.print(arr1[i][j]);
+//            }
+//            System.out.print("\n");
+//        }
+//
+//        for(int i=0; i < arr2.length; i++){
+//            for(int j=arr2[0].length-1; j >= 0; j--){
+//                if(i == j-1) {
+//                    arr1[i][j] = "O";
+//                }else {
+//                    arr1[i][j] = "o";
+//                }
+//                System.out.print(arr1[i][j]);
+//            }
+//            System.out.print("\n");
+//        }
 
-        for(int i=0; i < arr1.length; i++){
-            for(int j=0; j < arr1[0].length; j++){
-                if(i == j) {
-                    arr1[i][j] = "O";
-                }else {
-                    arr1[i][j] = "o";
-                }
-                System.out.print(arr1[i][j]);
-            }
-            System.out.print("\n");
-        }
-
-        for(int i=0; i < arr2.length; i++){
-            for(int j=arr2[0].length-1; j >= 0; j--){
-                if(i == j-1) {
-                    arr1[i][j] = "O";
-                }else {
-                    arr1[i][j] = "o";
-                }
-                System.out.print(arr1[i][j]);
-            }
-            System.out.print("\n");
-        }
+        //방법 3. 미완..
+//        boolean chg = true;
+//        int cnt = 0;
+//
+//        for(int i=0; i < 9; i++){ // 나중에 while 로 변경
+//            cnt++;
+//            for(int j =0; j < 5; j ++){
+//                if(i == j && chg){ // 정방향
+//                    System.out.print("O");
+//                }else if(i == j-1 && !chg){ // 역방향
+//                    System.out.print("O");
+//                }else {
+//                    System.out.print("o");
+//                }
+//            }
+//            System.out.print("\n");
+//            if(cnt % 5 ==0) {
+//                System.out.println("역방향으로 바꾸ㅓ!");
+//                chg = false;
+//            }
+//            if(cnt % 9 ==0) {
+//                System.out.println("정방향으로 바꾸ㅓ!");
+//                chg = true;
+//            }
+//        }
 
     }
 }
